d3f6b6915b1bf675f02be4f3bdf40a14bdd58f9a
Thiago47-git <th.ignacio47@gmail.com>
2020-11-26 22:58:55 -0300

    Atividade Foreach

---
+++ Atividade_Foreach/Atividade_Foreach/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Atividade_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Questao 1
+            int[] num = new int[6] { 1, 2, 3, 4, 5, 6 };
+
+            foreach (int n in num)
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine(); 
+            // Questao 2
+            string[] nome = new string[6] { "Thiago", "Bianca", "Diogo", "Diego", "Valdecy", "Aderli" };
+
+            foreach (string n in nome)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
