61905071c758e76c5452cfd7a46d325a92a6929c
Thiago47-git <th.ignacio47@gmail.com>
2020-11-28 15:12:13 -0300

    Vetor POO

---
+++ Atividade_19/Atividade_19/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Atividade_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Quartos[] x = new Quartos[10];
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Aluguel #"+ i +":");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine()); 
+
+                x[quarto] = new Quartos(nome, email, quarto);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(x[i]);
+
+
+            }
+        }
+    }
+}
---
+++ Atividade_19/Atividade_19/Quartos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade_19
+{
+    class Quartos
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        
+        public Quartos (string nome, string email, int quarto)
+        {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+        public override string ToString()
+        {
+            return Quarto+": "+Nome+", "+Email;
+        }
+    }
+}
