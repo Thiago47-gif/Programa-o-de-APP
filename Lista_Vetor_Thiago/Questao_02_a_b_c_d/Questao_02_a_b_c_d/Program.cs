035782566120fe9ffe8b49f77bbe49e744864022
Thiago47-gif <th.ignacio47@gmail.com>
2020-12-17 17:14:33 -0300

    Lista questao 2

---
+++ Lista_Vetor_Thiago/Questao_02_a_b_c_d/Questao_02_a_b_c_d/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Questao_02_a_b_c_d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[6];
+
+            A[0] = 1;
+            A[1] = 0;
+            A[2] = 5;
+            A[3] = -2;
+            A[4] = -5;
+            A[5] = 7;
+
+            int valor = int.Parse(Console.ReadLine());
+            int soma = valor + A[0] + A[1] + A[5];
+            Console.WriteLine(soma);
+
+            A[4] = 100; 
+
+            foreach (int i in A)
+            {
+                Console.WriteLine(i);
+            } 
+        }
+    }
+}
