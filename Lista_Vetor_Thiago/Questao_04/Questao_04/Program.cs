2b0fda20caaedbbfdb91f12f84659b6c0949ad1e
Thiago47-gif <th.ignacio47@gmail.com>
2020-12-17 21:25:58 -0300

    Lista questao 4

---
+++ Lista_Vetor_Thiago/Questao_04/Questao_04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Questao_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[8];
+
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write($"\n#{i}: ");
+                n[i] = int.Parse(Console.ReadLine());
+            } 
+            Console.WriteLine("Digite o valor de x e y (1 à 8):");
+            Console.Write("X = ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Y = ");
+            int y = int.Parse(Console.ReadLine());
+            int soma = n[x] + n[y];
+            Console.WriteLine($"\nSoma: {soma}");
+        }
+    }
+}
